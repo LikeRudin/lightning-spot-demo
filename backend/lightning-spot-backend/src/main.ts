ab9f0cfeabb0d2dee74f9e1c787585c50576a8cb
Young Jin <likeRudin@gmail.com>
2025-03-26 16:30:59 +0900

    chore: .github파일 복원

--- backend/lightning-spot-backend/src/main.ts
+++ backend/lightning-spot-backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const user = await prisma.user.findMany({
+    take: 10,
+  });
+  console.log(`test array: ${JSON.stringify(user)}`);
 }
 bootstrap();
